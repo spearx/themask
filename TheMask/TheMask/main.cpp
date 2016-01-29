77fdf3de3fcb96d60733c66b2f46a79d7a43fef1
Spear <spear@codernet.es>
2016-01-29 18:16:16 +0100

    first commit

---
+++ TheMask/TheMask/main.cpp
@@ -0,0 +1,16 @@
+// Game Jam 2016 Main - Barcelona - Ruben / Ramon / Arturo
+//
+
+#include "abathur.h"
+
+int main()
+{
+	Abathur::Init("The Incredible Mask", 640, 480, false);
+
+
+
+	Abathur::Run();
+
+    return 0;
+}
+
